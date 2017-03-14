2f6c6c879896082d91ee0cae7408d2a170f6484d
Jaelson <jaelsonjunior99@gmail.com>
2017-03-14 16:10:00 -0300

    Update signup action

--- src/controllers/authentication.js
+++ src/controllers/authentication.js
@@ -3,5 +3,25 @@ const User = require('../models/user');
 
 exports.signup = function (req, res, next) {
   const {email, password} = req.body;
-  res.send({sucess: 'true'});
+
+  if(!email || !password) {
+    return res.status(422).send({error: 'Email and password must be provided.'})
+  }
+
+  User.findOne({email}, function(err, foundUser){
+    if(err) { return next(err); }
+
+    if(foundUser) {
+      return res.status(422).send({error: 'This email has already been used.'})
+    }
+
+    const newUser = new User({email,password});
+
+    newUser.save(function(err) {
+      if(err) { return next(err); }
+
+      res.status(201).json(newUser)
+    })
+
+  })
 };
--- src/models/user.js
+++ src/models/user.js
@@ -1,10 +1,33 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 // User Model Definition
 const userSchema = new Schema({
   email: {type: String, unique: true, lowercase: true},
   password: String
+});
+
+// Before saving a model
+userSchema.pre('save', function(next) {
+  // Get access to the user model
+  const user = this;
+
+  // Generate a salt
+  bcrypt.genSalt(10, function(err, salt) {
+    if(err) { return next(err); }
+
+    // Encrypt the password using the generated salt
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if(err) { return next(err); }
+
+      // Use the encrypted password
+      user.password = hash;
+
+      // Go ahead and save the user model
+      next();
+    })
+  })
 });
 
 // Model Class Creation
