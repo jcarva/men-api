4c70c54d687fe013e1dc6e3a403012157d9975a0
Jaelson <jaelsonjunior99@gmail.com>
2017-03-21 17:41:33 -0300

    Apply authentication middlewares to protect some resources

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,7 +1,11 @@
+// Local 'Middleware Authentication' helper
+const {localAuthentication} = require('../helpers/authentication');
+
 // Authentication Controller
 const AuthenticationController = require('../controllers/authentication');
 
 // Authentication Routes
 module.exports = function (app) {
-  app.post('/auth/signup', AuthenticationController.signup);
+  app.post('/auth/signup', AuthenticationController.signUp);
+  app.post('/auth/signin', localAuthentication, AuthenticationController.signIn);
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,14 @@
+// JWT 'Middleware Authentication' helper
+const {jwtAuthentication} = require('../helpers/authentication');
+
 // Routes
 const AuthenticationRoutes = require('./authentication');
 
 // Router
 module.exports = function (app) {
   AuthenticationRoutes(app);
+  // Protected resources
+  app.use('*', jwtAuthentication, function (req, res) {
+    res.send({Authentication: 'Sucess'});
+  });
 };
